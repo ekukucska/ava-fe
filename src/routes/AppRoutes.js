45bf414b11687de07dad7fbc981d61c43c085809
eduard.kukucska <andreikukucska@gmail.com>
2024-10-13 17:39:46 +0300

    NotFound page & route

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,10 +1,11 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Studies from "../pages/StudiesPage/StudiesPage.jsx";
-import Subjects from "../pages/SubjectsPage/SubjectsPage.jsx";
-import Events from "../pages/EventsPage/EventsPage.jsx";
-import EventsDetailsPage from "../pages/EventsDetailsPage/EventsDetailsPage.jsx";
-import MainLayout from "../layouts/MainLayout/MainLayout.jsx";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Studies from '../pages/StudiesPage/StudiesPage.jsx';
+import Subjects from '../pages/SubjectsPage/SubjectsPage.jsx';
+import Events from '../pages/EventsPage/EventsPage.jsx';
+import EventsDetailsPage from '../pages/EventsDetailsPage/EventsDetailsPage.jsx';
+import NotFoundPage from '../pages/NotFoundPage/NotFoundPage.jsx';
+import MainLayout from '../layouts/MainLayout/MainLayout.jsx';
 
 function AppRoutes() {
   return (
@@ -15,6 +16,7 @@ function AppRoutes() {
         <Route path="subjects" element={<Subjects />} />
         <Route path="events" element={<Events />} />
         <Route path="events/events-details" element={<EventsDetailsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
   );
