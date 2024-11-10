d75482f05703987dc16c2ea7e091cc8fb4a6ef78
eduard.kukucska <andreikukucska@gmail.com>
2024-11-10 11:10:26 +0200

    Show all subjects button

--- src/state/EventsTypesContext.jsx
+++ src/state/EventsTypesContext.jsx
@@ -4,14 +4,19 @@ import PropTypes from 'prop-types';
 const EventTypesContext = createContext({
   selectedEventsTypes: [],
   setSelectedEventsTypes: () => {},
+  showAllsubjects: false,
+  setShowAllSubjects: () => {},
 });
 
 export const EventTypesContextProvider = ({ children }) => {
   const [selectedEventsTypes, setSelectedEventsTypes] = useState([]);
+  const [showAllSubjects, setShowAllSubjects] = useState(false);
 
   const context = {
     selectedEventsTypes,
     setSelectedEventsTypes,
+    showAllSubjects,
+    setShowAllSubjects,
   };
 
   return (
