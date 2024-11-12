8610f99c1767b401163dbc1c6bc7471614600d81
eduard.kukucska <andreikukucska@gmail.com>
2024-11-12 12:41:22 +0200

    Made events plot dynamic

--- src/components/EventsPlot/EventsPlot.jsx
+++ src/components/EventsPlot/EventsPlot.jsx
@@ -1,69 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Plot from 'react-plotly.js';
 import { CardContent, Box } from '@mui/material';
 
-const EventsPlot = ({ reset }) => {
-  const [plotData, setPlotData] = useState([
-    {
-      x: [1, 2, 3, 4, 5],
-      y: [2, 6, 3, 7, 4],
-      type: 'scatter',
-      mode: 'lines+markers',
-      marker: { color: '#30475D' },
-      name: 'CGM',
-    },
-    {
-      type: 'bar',
-      x: [1, 2, 3],
-      y: [2, 5, 3],
-      marker: { color: 'lightgrey' },
-      name: 'Anomalies',
-    },
-    {
-      type: 'bar',
-      x: [1.5, 0.7, 3.5],
-      y: [4, 4, 4],
-      marker: { color: '#9F1853' },
-      name: 'Event type 1',
-    },
-  ]);
-
-  // UseEffect to reset the plot when the reset prop changes
-  useEffect(() => {
-    if (reset) {
-      setPlotData([
-        {
-          x: [1, 2, 3, 4, 5],
-          y: [2, 6, 3, 7, 4],
-          type: 'scatter',
-          mode: 'lines+markers',
-          marker: { color: '#30475D' },
-          name: 'CGM',
-        },
-        {
-          type: 'bar',
-          x: [1, 2, 3],
-          y: [2, 5, 3],
-          marker: { color: 'lightgrey' },
-          name: 'Anomalies',
-        },
-        {
-          type: 'bar',
-          x: [1.5, 0.7, 3.5],
-          y: [4, 4, 4],
-          marker: { color: '#9F1853' },
-          name: 'Event Type 1',
-        },
-      ]);
-    }
-  }, [reset]);
-
+const EventsPlot = ({ dataSets }) => {
   return (
     <Box sx={{ width: '100%' }}>
       <CardContent>
         <Plot
-          data={plotData}
+          data={dataSets}
           layout={{
             // title: 'A Fancy Plot',
             autosize: true, // Enable auto-resizing
@@ -78,7 +23,7 @@ const EventsPlot = ({ reset }) => {
 };
 
 EventsPlot.propTypes = {
-  reset: PropTypes.bool.isRequired,
+  dataSets: PropTypes.bool.isRequired,
 };
 
 export default EventsPlot;
